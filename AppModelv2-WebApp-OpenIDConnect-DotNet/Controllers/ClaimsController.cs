ed7c33bcce1908f13aa23f574e7e8076f16eb9c8
Roberto Borges <roborges@microsoft.com>
2022-01-13 08:56:40 -0500

    update with MFA validation

--- AppModelv2-WebApp-OpenIDConnect-DotNet/Controllers/ClaimsController.cs
+++ AppModelv2-WebApp-OpenIDConnect-DotNet/Controllers/ClaimsController.cs
@@ -1,12 +1,6 @@
-﻿using Microsoft.IdentityModel.Protocols;
-using Microsoft.IdentityModel.Protocols.OpenIdConnect;
-using Microsoft.IdentityModel.Tokens;
-using System;
-using System.Collections.Generic;
-using System.Globalization;
+﻿using System.Collections.Generic;
 using System.IdentityModel.Tokens.Jwt;
 using System.Linq;
-using System.Security.Claims;
 using System.Web;
 using System.Web.Mvc;
 
@@ -23,56 +17,15 @@ namespace AppModelv2_WebApp_OpenIDConnect_DotNet.Controllers
         {
             var userClaims = User.Identity as System.Security.Claims.ClaimsIdentity;
 
-            var stsDiscoveryEndpoint = String.Format(CultureInfo.InvariantCulture, "https://login.microsoftonline.com/{0}/.well-known/openid-configuration", "b9f303d6-1732-4928-95bd-26243f271827");
-            var configManager = new ConfigurationManager<OpenIdConnectConfiguration>(stsDiscoveryEndpoint, new OpenIdConnectConfigurationRetriever());
-            var config = configManager.GetConfigurationAsync();
-
             var result = Request.GetOwinContext().Authentication.AuthenticateAsync("Cookies");
-            string token = result.Result.Properties.Dictionary["id_token"];
+            string token = result.Result.Properties.Dictionary["access_token"];
 
+            var TokenInfo = new Dictionary<string, string>();
             var handler = new JwtSecurityTokenHandler();
             var jwtSecurityToken = handler.ReadJwtToken(token);
+            var claims = jwtSecurityToken.Claims.ToList();
 
-            TokenValidationParameters tokenValidationParameters = new TokenValidationParameters
-            {
-                IssuerSigningKey = jwtSecurityToken.SecurityKey,
-                RequireExpirationTime = true,
-                RequireAudience = false,
-                ValidateLifetime = true,
-                ValidateAudience = true,
-                ValidateIssuer = true,
-                AuthenticationType = "Test"
-            };
-
-            if (ValidateToken(token, tokenValidationParameters))
-            {
-                var TokenInfo = new Dictionary<string, string>();
-                jwtSecurityToken = handler.ReadJwtToken(token);
-                var claims = jwtSecurityToken.Claims.ToList();
-
-                foreach (var claim in claims)
-                {
-                    TokenInfo.Add(claim.Type, claim.Value);
-                }
-
-                string sub = jwtSecurityToken.Subject;
-                string iss = jwtSecurityToken.Issuer;
-                DateTime iat = jwtSecurityToken.IssuedAt;
-                List<string> audiences = new List<string>(jwtSecurityToken.Audiences);
-                DateTime exp = jwtSecurityToken.ValidTo;
-                string bar;
-                bool ifBar = TokenInfo.TryGetValue("foo", out bar);
-                Console.WriteLine("Subject: " + sub);
-                Console.WriteLine("Issuer: " + iss);
-                Console.WriteLine("Issued At: " + iat);
-                foreach (var member in audiences)
-                {
-                    Console.WriteLine("Audience: " + member);
-                }
-                Console.WriteLine("Expiration: " + exp);
-                Console.WriteLine("foo: " + bar);
-            }
-            Console.ReadLine();
+            userClaims.AddClaims(claims);
 
             //You get the user’s first and last name below:
             ViewBag.Name = userClaims?.FindFirst("name")?.Value;
@@ -86,25 +39,9 @@ namespace AppModelv2_WebApp_OpenIDConnect_DotNet.Controllers
             // TenantId is the unique Tenant Id - which represents an organization in Azure AD
             ViewBag.TenantId = userClaims?.FindFirst("http://schemas.microsoft.com/identity/claims/tenantid")?.Value;
 
+            ViewBag.MFA = ((System.Security.Claims.ClaimsIdentity)System.Security.Claims.ClaimsPrincipal.Current.Identity).HasClaim("amr", "mfa");
+
             return View();
         }
-
-        private bool ValidateToken(string token, TokenValidationParameters tvp)
-        {
-            try
-            {
-                var handler = new JwtSecurityTokenHandler();
-
-                SecurityToken securityToken;
-                ClaimsPrincipal principal = handler.ValidateToken(token, tvp, out securityToken);
-                return true;
-            }
-            catch (Exception ex)
-            {
-                Console.WriteLine(ex.Message);
-                return false;
-            }
-        }
-
     }
 }
